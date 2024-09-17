31b63f363e7b1d4bdd7f25940686c23bd66ecb0f
GGufficiale <germangranovskii@mail.ru>
2024-09-17 13:13:02 +0300

    Создан метод для создания stripe-продукта

--- users/services.py
+++ users/services.py
@@ -11,7 +11,7 @@ stripe.api_key = STRIPE_API_KEY
 
 def create_stripe_product(instance):
     """Создаем stripe продукт"""
-    title_product = f'{instance.paid_course}' if instance.paid_course else f'{instance.paid_lesson}'
+    title_product = f'{instance.course_paid}' if instance.course_paid else f'{instance.lesson_paid}'
     stripe_product = stripe.Product.create(name=f"{title_product}")
     return stripe_product.get('id')
 
