346908aa5ae6a6f28a2a61b255f998031412b971
GGufficiale <germangranovskii@mail.ru>
2024-09-27 20:39:08 +0300

    Корректура рассылки писем юзерам об обновлении материалов курса

--- courses_lessons/tasks.py
+++ courses_lessons/tasks.py
@@ -2,15 +2,23 @@ from config.settings import EMAIL_HOST_USER
 from django.core.mail import send_mail
 from celery import shared_task
 
+from courses_lessons.models import Subscription
+
 
 @shared_task
-def send_email(email, ):
+def send_email(course_id):
     """
     Отправляет письмо с уведомлением об изменении курса.
     """
-    send_mail(
-        subject="Обновление",
-        message="Курс по Вашей подписке обновлен",
-        from_email=EMAIL_HOST_USER,
-        recipient_list=[email],
-    )
+    subs = Subscription.objects.filter(course=course_id, status=True)
+    for sub in subs:
+        course = sub.course
+        user = sub.owner
+        send_mail(
+            subject=f'{course} обновился',
+            message=f'{course} обновился',
+            from_email=EMAIL_HOST_USER,
+            recipient_list=[user.email],
+            fail_silently=False
+        )
+        print(f'Письмо отправлено пользователю {user.email}')
